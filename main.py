a5c05bbd81a05f74a64e04e9d95e9bf1c94ec162
Chris Wren <chris+github@drwren.com>
2026-03-17 09:50:06 -0400

    add group member store
    for persistant tracking of group additions
    required for responding to new arrivals
    
    todo: add a mechanism to prume old groups

--- main.py
+++ main.py
@@ -1,10 +1,44 @@
+from dotenv import load_dotenv
 import logging
 import os
-from dotenv import load_dotenv
 from signalbot import SignalBot, Config, Command, Context, MessageType, SQLiteConfig, enable_console_logging
+import sqlite3
 
-group_members = {}
 logger = logging.getLogger("welcomebot")
+
+class GroupMembers():
+    def __init__(self):
+        self.con = sqlite3.connect("group_members.db")
+        cur = self.con.cursor()
+        cur.execute("""   
+             CREATE TABLE IF NOT EXISTS group_members (
+                 group_id TEXT,
+                 member_id TEXT
+             );
+        """)
+        self.con.commit()
+        cur.close()
+
+    def __del__(self):
+        self.con.close()
+
+    def get(self, group):
+        cur = self.con.cursor()
+        res = cur.execute(f'SELECT * FROM group_members WHERE group_id = "{group}"')
+        rows = res.fetchall()
+        cur.close()
+        return [ row[1] for row in rows ]
+
+    def put(self, group, members):
+        cur = self.con.cursor()
+        cur.execute(f'DELETE FROM group_members WHERE group_id = "{group}"')
+        self.con.commit()
+        rows = [ (group, member) for member in members ]
+        cur = self.con.cursor()
+        cur.executemany("INSERT INTO group_members VALUES(?, ?)", rows)
+        self.con.commit()
+        cur.close()
+
 
 class CNCCommand(Command):
     def __init__(self, manager, cnc):
@@ -28,9 +62,10 @@ class CNCCommand(Command):
             await context.send(reply)
 
 class SocialCommand(Command):
-    def __init__(self, manager, cnc):
+    def __init__(self, manager, cnc, gm):
         self.manager = manager
         self.cnc = cnc
+        self.gm = gm
 
     async def handle(self, context: Context) -> None:
         if context.message.group == self.cnc:
@@ -59,29 +94,36 @@ class SocialCommand(Command):
 
         if context.message.type == MessageType.GROUP_UPDATE_MESSAGE:
             logger.info("social processing group update")
-            reply = ""
-            group = self.bot._groups_by_internal_id[context.message.group]
-            reply = f'saw a group update for {group["name"]}\n'
-            if context.message.group in group_members:
-                for member in group["members"]:
-                    logger.debug(f'  looking for {member}')
-                    if member not in group_members[context.message.group]:
+            post_group = self.bot._groups_by_internal_id[context.message.group]
+            reply = f'saw a group update for {post_group["name"]}\n'
+            prev_members = self.gm.get(context.message.group)
+            if prev_members:
+                for member in post_group["members"]:
+                    logger.debug(f'  looking for {member} in old group')
+                    if member not in prev_members:
                         logger.debug("  found a new member of the group")
                         reply += f'welcome: {member}\n'
+                for member in prev_members:
+                    logger.debug(f'  looking for {member} in new group')
+                    if member not in post_group["members"]:
+                        logger.debug("  a member left the group")
+                        reply += f'goodbye: {member}\n'
             else:
                 logger.debug("  found a new group")
-            group_members[context.message.group] = group["members"]
+                reply += f'this is a new group for me: hello all!\n'
+            self.gm.put(context.message.group, post_group["members"])
             logger.debug("social acknowledging group change")
             await context.send(reply)
             return
 
 def main():
+    gm = GroupMembers()
     bot = SignalBot(
         Config(
             signal_service=os.environ["SIGNAL_SERVICE"],
             phone_number=os.environ["PHONE_NUMBER"],
             storage=SQLiteConfig(
-                sqlite_db='signalbot_internal_state.sql',
+                sqlite_db='signalbot_internal_state.db',
             )
         )
     )
@@ -90,7 +132,7 @@ def main():
     manager = os.environ["WELCOME_MANAGER"]
 
     bot.register(CNCCommand(manager, cnc), groups=[cnc]) # monitor CNC group
-    bot.register(SocialCommand(manager, cnc)) # monitor other groups
+    bot.register(SocialCommand(manager, cnc, gm)) # monitor other groups
     bot.start()
     logger.info("bot started")
 
