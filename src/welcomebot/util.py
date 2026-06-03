935d632754d5f5e2be3d2b7ff093f8e9121370eb
Chris Wren <chris+github@drwren.com>
2026-06-03 16:16:18 -0400

    catch nonetype return from groups
    can trigger this by sending a DM to the bot,
    probably shouldn't be updating groups in that case

--- src/welcomebot/util.py
+++ src/welcomebot/util.py
@@ -1,24 +1,27 @@
 async def update_group (logger, bot, context, store):
     logger.info("updating group information")
     post_group = bot.get_group(context.message.group)
-    prev_members = store.get_members(context.message.group)
-    new_member = False
-    if prev_members:
-        for member in post_group["members"]:
-            logger.debug(f'  looking for {member} in old group')
-            if member not in prev_members:
-                logger.debug("  found a new member of the group")
-                new_member = True
-        for member in prev_members:
-            logger.debug(f'  looking for {member} in new group')
-            if member not in post_group["members"]:
-                logger.debug("  a member left the group")
-    else:
-        logger.debug("  found a new group")
-        # TODO post introduction
+    if post_group and "members" in post_group:
+        prev_members = store.get_members(context.message.group)
+        new_member = False
+        if prev_members:
+            for member in post_group["members"]:
+                logger.debug(f'  looking for {member} in old group')
+                if member not in prev_members:
+                    logger.debug("  found a new member of the group")
+                    new_member = True
+            for member in prev_members:
+                logger.debug(f'  looking for {member} in new group')
+                if member not in post_group["members"]:
+                    logger.debug("  a member left the group")
+        else:
+            logger.debug("  found a new group")
+            # TODO post introduction
 
-    # update member cache
-    store.put_members(context.message.group, post_group["members"])
+        # update member cache
+        store.put_members(context.message.group, post_group["members"])
+
+    logger.info("purging obsolete groups")
     valid_group_ids = [ g["internal_id"] for g in bot.groups ]
     store.retain_only(valid_group_ids)
 
