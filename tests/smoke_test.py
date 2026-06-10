39ae172e2bd39cb896cb329af7f0c4125faa576a
Chris Wren <chris+github@drwren.com>
2026-06-10 11:38:29 -0400

    fix smoke test
    missing reminder service arg tro cnc constructor

--- tests/smoke_test.py
+++ tests/smoke_test.py
@@ -4,9 +4,11 @@ from welcomebot import store
 from welcomebot import cnc
 from welcomebot import motd
 from welcomebot import main
+from welcomebot import periodic
 
 logger = logging.getLogger("smoke")
 bot_store = store.BotStore(logger, db=":memory:")
-cnc.CNCCommand(logger, [], "foo", bot_store)
+reminders = periodic.Reminders(logger, [], bot_store)
+cnc.CNCCommand(logger, [], "foo", bot_store, reminders)
 motd.MotDCommand(logger, "foo", bot_store)
 assert main.main
