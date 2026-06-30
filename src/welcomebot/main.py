6ed7217fdad49573c1ed2153e7f60d3098eb32b5
Chris Wren <chris+github@drwren.com>
2026-06-29 20:21:47 -0400

    make reminder time(s) tunable from environment

--- src/welcomebot/main.py
+++ src/welcomebot/main.py
@@ -43,7 +43,13 @@ def loop():
     bot.register(cnc.CNCCommand(logger, managers, cnc_id, bot_store, reminders), groups=[cnc_id]) # monitor other groups
     bot.register(motd.MotDCommand(logger, cnc_id, bot_store)) # monitor other groups
     bot.scheduler.add_job(lubdub, trigger="interval", seconds=60, coalesce=True, max_instances=1)
-    bot.scheduler.add_job(remind, args=[reminders], trigger='cron', hour='13', coalesce=True, max_instances=1)
+    bot.scheduler.add_job(
+        remind,
+        args=[reminders],
+        trigger='cron',
+        hour=os.environ.get('REMINDER_TIMES', '13'),
+        coalesce=True,
+        max_instances=1)
 
     logger.info("bot started")
     bot.start()
